f04ba6c67c2f39e9e6127f2a4819dc639cd3eaa0
abkraju <rajurbk12@gmail.com>
2022-06-03 16:32:08 +0530

    ContactSection

---
+++ TRSskillfill/src/main/java/Com/TRSConsultancyServices/TRSskillfill/ContactUs.java
@@ -0,0 +1,33 @@
+package Com.TRSConsultancyServices.TRSskillfill;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import java.time.Duration;
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class ContactUs extends Services {
+	@Test
+	void Contact() {
+		WebElement contactdetails = driver.findElement(By.xpath("//a[@href='#contact']"));
+		contactdetails.click();
+		boolean contactclick = contactdetails.isEnabled();
+		System.out.println("Contact is Clickable :" + contactclick);
+		WebElement addressdetails = driver.findElement(By.xpath("(//div[@class='col-lg-4 col-md-6'])[1]"));
+		Assert.assertTrue(addressdetails.isDisplayed());
+	    System.out.println("Address is Displayed:"+addressdetails.isDisplayed());
+		WebElement emailiddetails = driver.findElement(By.xpath("(//div[@class='col-lg-4 col-md-6'])[2]"));
+		Assert.assertTrue(emailiddetails.isDisplayed());
+		System.out.println("EmailID is Displayed:" + emailiddetails.isDisplayed());
+		WebElement contactnumber = driver.findElement(By.xpath("(//div[@class='col-lg-4 col-md-6'])[3]"));
+		Assert.assertTrue(contactnumber.isDisplayed());
+		System.out.println("Contact Number is Displayed:" + contactnumber.isDisplayed());
+	}
+}
+
+
---
+++ TRSskillfill/src/main/java/Com/TRSConsultancyServices/TRSskillfill/LeaveMessage.java
@@ -0,0 +1,24 @@
+package Com.TRSConsultancyServices.TRSskillfill;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebElement;
+import org.testng.annotations.Test;
+
+public class LeaveMessage extends ContactUs {
+	@Test
+	void PerDetails() {
+		WebElement detailsname = driver.findElement(By.xpath("//input[@name='name']"));
+		detailsname.sendKeys("Kuppuraju");
+		detailsname.sendKeys(Keys.TAB);
+		WebElement detailsemail = driver.findElement(By.xpath("//input[@type='email']"));
+		detailsemail.sendKeys("rajurbk12@gmail.com");
+		detailsemail.sendKeys(Keys.TAB);
+		WebElement detailsmsg = driver.findElement(By.xpath("//textarea[@name='message']"));
+		detailsmsg.sendKeys("I am Interested");
+		detailsmsg.sendKeys(Keys.ENTER);
+		WebElement detailssubmit = driver.findElement(By.xpath("//button[@class='main-btn']"));
+		detailssubmit.click();
+		System.out.println("All Required Details Submited Successfully in Leave a Message Section");
+	}
+}
